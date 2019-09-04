920b202f6c4cb7be8f0c44f777b7469065b35969
Richard Davey <rich@photonstorm.com>
2019-09-04 02:26:42 +0100

    First TS module test

---
+++ src/001/test001.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Game_1 = require("../../../phaser4/src/Game");
+let canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+new Game_1.default(canvas);
---
+++ src/001/test001.ts
@@ -0,0 +1,7 @@
+import Game from "../../../phaser4/src/Game";
+
+let canvas:HTMLCanvasElement = document.createElement('canvas');
+
+document.body.appendChild(canvas);
+
+new Game(canvas);
