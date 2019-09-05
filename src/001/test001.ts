3a8b7951c5f7f75f7b43229423070c7e5b711175
Richard Davey <rich@photonstorm.com>
2019-09-05 03:56:48 +0100

    Testing self loading

--- src/001/test001.ts
+++ src/001/test001.ts
@@ -1,9 +1,15 @@
 import * as Phaser from 'phaser'
 
 let game = new Phaser.Game();
-let loader = new Phaser.Loader();
 
 game.draw('Loading ...');
+
+Phaser.ImageFile('logo', '../assets/logo.png').load().then((file) => {
+    game.drawImage(file.data, 100, 100);
+});
+
+/*
+let loader = new Phaser.Loader();
 
 loader.image('logo', '../assets/logo.png')
     .then((file) => {
@@ -17,3 +23,4 @@ loader.image('logo', '../assets/logo.png')
     });
 
 loader.start();
+*/
