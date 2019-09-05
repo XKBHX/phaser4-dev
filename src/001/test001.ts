8eb3e7636f9ce168f9c1854b7ae49b947e9a9bc1
Richard Davey <rich@photonstorm.com>
2019-09-05 22:33:36 +0100

    Added Test 002

--- src/001/test001.ts
+++ src/001/test001.ts
@@ -1,11 +1,9 @@
-// import * as Phaser from 'phaser'
-
 import { Game } from 'phaser/src/Game';
 import { ImageFile } from 'phaser/src/loader/filetypes/ImageFile';
 
 let game = new Game();
 
-game.draw('Loading ...');
+game.draw('Phaser 4 Test 001');
 
 ImageFile('logo', '../assets/logo.png').load().then((file) => {
 
@@ -17,36 +15,3 @@ ImageFile('logo', '../assets/logo.png').load().then((file) => {
         game.drawImage(file.data, x, y);
     }
 });
-
-// import { Game, Loader } from 'phaser';
-
-// let game = new Phaser.Game();
-
-/*
-Phaser.Loader.FileTypes.ImageFile('logo', '../assets/logo.png').load().then((file) => {
-    game.drawImage(file.data, 100, 100);
-    game.drawImage(file.data, 200, 200);
-    game.drawImage(file.data, 300, 300);
-});
-*/
-
-// Phaser.ImageFile('logo', '../assets/logo.png').load().then((file) => {
-//     game.drawImage(file.data, 100, 100);
-// });
-
-/*
-let loader = new Phaser.Loader();
-
-loader.image('logo', '../assets/logo.png')
-    .then((file) => {
-        console.log('done?!');
-        console.log(file);
-
-        game.drawImage(file.data);
-    })
-    .catch(() => {
-        console.log('failed');
-    });
-
-loader.start();
-*/
---
+++ src/002/test002.ts
@@ -0,0 +1,24 @@
+import * as Phaser from 'phaser';
+
+let game = new Phaser.Game();
+let loader = new Phaser.Loader.LoaderPlugin();
+
+game.draw('Phaser 4 Test 002');
+
+loader.image('logo', '../assets/logo.png')
+    .then((file) => {
+
+        for (let i = 0; i < 10; i++)
+        {
+            let x = Math.random() * 700;
+            let y = Math.random() * 500;
+    
+            game.drawImage(file.data, x, y);
+        }
+
+    })
+    .catch(() => {
+        console.log('failed');
+    });
+
+loader.start();
