861ccb880cd7f2abfb99280e51ada00d7902c48a
Richard Davey <rich@photonstorm.com>
2019-09-06 14:20:57 +0100

    New build scripts

---
+++ scripts/build.js
@@ -0,0 +1,62 @@
+let fs = require('fs-extra');
+let parseArgs = require('minimist');
+let { exec } = require('child_process');
+
+let source = './src/';
+
+//  Can do: `npm run build -- 001 (or just npm run build for it to build the most recent folder)
+let argv = parseArgs(process.argv);
+
+let folder = argv._[2];
+
+if (!folder)
+{
+    //  If no argument is given, look for the most recent folder and use that
+    function getDirectories (path)
+    {
+        return fs.readdirSync(path).filter(function (file)
+        {
+            return fs.statSync(path + '/' + file).isDirectory();
+        });
+    }
+
+    let inputDirs = getDirectories(source);
+    let maxDir = 0;
+
+    for (let i = 0; i < inputDirs.length; i++)
+    {
+        let d = parseInt(inputDirs[i]);
+
+        if (!isNaN(d))
+        {
+            maxDir = Math.max(maxDir, d);
+        }
+    }
+
+    folder = maxDir.toString().padStart(3, '0');
+}
+
+let dest = source + folder;
+
+if (fs.existsSync(dest))
+{
+    // console.log('building', dest);
+
+    const w = exec('rollup -c ' + dest + '/rollup.config.js');
+
+    w.stdout.on('data', (data) => {
+        console.log(data);
+    });
+
+    w.stderr.on('data', (data) => {
+        console.log(data);
+    });
+
+    // w.on('close', (code) => {
+    //     console.log(`child process exited with code ${code}`);
+    // });
+}
+else
+{
+    console.log('Failed to find test', dest);
+}
---
+++ scripts/test.js
@@ -0,0 +1,52 @@
+let fs = require('fs-extra');
+let parseArgs = require('minimist');
+let { execSync } = require('child_process');
+
+let source = './src/';
+
+//  Can do: `npm run build -- 001 (or just npm run build for it to build the most recent folder)
+let argv = parseArgs(process.argv);
+
+let folder = argv._[2];
+
+if (!folder)
+{
+    //  If no argument is given, look for the most recent folder and use that
+    function getDirectories (path)
+    {
+        return fs.readdirSync(path).filter(function (file)
+        {
+            return fs.statSync(path + '/' + file).isDirectory();
+        });
+    }
+
+    let inputDirs = getDirectories(source);
+    let maxDir = 0;
+
+    for (let i = 0; i < inputDirs.length; i++)
+    {
+        let d = parseInt(inputDirs[i]);
+
+        if (!isNaN(d))
+        {
+            maxDir = Math.max(maxDir, d);
+        }
+    }
+
+    folder = maxDir.toString().padStart(3, '0');
+}
+
+let dest = source + folder;
+
+if (fs.existsSync(dest))
+{
+    console.log('Running through tsc', dest);
+
+    let s = execSync('tsc --project ' + dest);
+
+    console.log(s.toString());
+}
+else
+{
+    console.log('Failed to find test', dest);
+}
