80d90d29fe34074595c576ffcb30942aa0b2dee1
Richard Davey <rich@photonstorm.com>
2019-11-04 17:13:11 +0000

    Updated api

--- src/010/test010.ts
+++ src/010/test010.ts
@@ -6,6 +6,7 @@ layout(location=0) in vec4 position;
 layout(location=1) in vec3 color;
 
 out vec3 vColor; 
+
 void main() {
     vColor = color;
     gl_Position = position;
@@ -18,38 +19,37 @@ precision highp float;
 in vec3 vColor;
 
 out vec4 fragColor;
+
 void main() {
     fragColor = vec4(vColor, 1.0);
 }
 `;
 
-let app = new WebGL2Renderer(document.getElementById('game') as HTMLCanvasElement, {});
+const app = new WebGL2Renderer(document.getElementById('game') as HTMLCanvasElement);
 
-app.clearColor(0, 0, 0, 1);
+app.setClearColor(0, 0, 0, 1);
 
-let positions = app.createVertexBuffer(app.gl.FLOAT, 2, new Float32Array([
+const program = app.createProgram(vs, fs);
+
+const positions = app.createVertexBuffer(app.gl.FLOAT, 2, new Float32Array([
     -0.5, -0.5,
-     0.5, -0.5,
-     0.0, 0.5, 
+    0.5, -0.5,
+    0.0, 0.5, 
 ]));
 
-let colors = app.createVertexBuffer(app.gl.UNSIGNED_BYTE, 3, new Uint8Array([
+const colors = app.createVertexBuffer(app.gl.UNSIGNED_BYTE, 3, new Uint8Array([
     255, 0, 0,
     0, 255, 0,
     0, 0, 255
 ]));
 
-let triangleArray = app.createVertexArray().vertexAttributeBuffer(0, positions, {}).vertexAttributeBuffer(1, colors, { normalized: true });
+const vertexArray = app.createVertexArray();
 
-let program = app.createProgram(vs, fs);
+vertexArray.vertexAttributeBuffer(0, positions, {});
+vertexArray.vertexAttributeBuffer(1, colors, { normalized: true });
 
-let drawCall = app.createDrawCall(program, triangleArray);
+const drawCall = app.createDrawCall(program, vertexArray);
 
 app.clear();
 
 drawCall.draw();
-
-// program.delete();
-// positions.delete();
-// colors.delete();
-// triangleArray.delete();
