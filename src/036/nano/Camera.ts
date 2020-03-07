5abb45c010e6b2542ea64f63e58279ca5b7c212f
Richard Davey <rdavey@gmail.com>
2020-03-07 12:04:34 +0000

    Camera overhauled to use DisplayObject as base

--- src/036/nano/Camera.ts
+++ src/036/nano/Camera.ts
@@ -1,6 +1,8 @@
 import WebGLRenderer from './WebGLRenderer';
+import DisplayObject from './DisplayObject';
+import Scene from './Scene';
 
-export default class Camera
+export default class Camera extends DisplayObject
 {
     matrix: Float32Array;
     renderer: WebGLRenderer;
@@ -8,157 +10,61 @@ export default class Camera
     readonly width: number;
     readonly height: number;
 
-    private _x: number = 0;
-    private _y: number = 0;
-    private _rotation: number = 0;
-    private _scaleX: number = 1;
-    private _scaleY: number = 1;
-
-    constructor (renderer: WebGLRenderer, width?: number, height?: number)
+    constructor (scene: Scene, x: number = 0, y: number = 0)
     {
-        if (!width)
-        {
-            width = renderer.width;
-        }
+        super(scene, x, y);
 
-        if (!height)
-        {
-            height = renderer.height;
-        }
-
-        this.renderer = renderer;
+        this.renderer = scene.game.renderer;
 
         this.matrix = new Float32Array([ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ]);
 
-        this.width = width;
-        this.height = height;
+        this.setSize(this.renderer.width, this.renderer.height);
     }
 
-    private translate (x: number, y: number, z: number = 0)
+    update ()
+    {
+        if (this.dirty)
+        {
+            //  sync wt to mt
+            const mat = this.matrix;
+            const { a, b, c, d, tx, ty } = this.worldTransform;
+
+            const viewportW = this.renderer.width * this.originX;
+            const viewportH = this.renderer.height * this.originY;
+
+            mat[0] = a;
+            mat[1] = b;
+            mat[4] = c;
+            mat[5] = d;
+
+            //  combinates viewport translation + scrollX/Y
+
+            mat[12] = (a * -viewportW) + (c * -viewportH) + (viewportW + tx);
+            mat[13] = (b * -viewportW) + (d * -viewportH) + (viewportH + ty);
+
+            // mat[12] = viewportW + tx; // combines translation to center of viewport + scrollX
+            // mat[13] = viewportH + ty; // combines translation to center of viewport + scrollY
+            // this.translate(-viewportW, -viewportH);
+            // console.log(mat);
+
+            this.dirty = false;
+        }
+    }
+
+    /*
+    translate (x: number, y: number)
     {
         const matrix = this.matrix;
 
         const m00 = matrix[0];
         const m01 = matrix[1];
-        const m02 = matrix[2];
-        const m03 = matrix[3];
         const m10 = matrix[4];
         const m11 = matrix[5];
-        const m12 = matrix[6];
-        const m13 = matrix[7];
-        const m20 = matrix[8];
-        const m21 = matrix[9];
-        const m22 = matrix[10];
-        const m23 = matrix[11];
         const m30 = matrix[12];
         const m31 = matrix[13];
-        const m32 = matrix[14];
-        const m33 = matrix[15];
 
-        matrix[12] = m00 * x + m10 * y + m20 * z + m30;
-        matrix[13] = m01 * x + m11 * y + m21 * z + m31;
-        matrix[14] = m02 * x + m12 * y + m22 * z + m32;
-        matrix[15] = m03 * x + m13 * y + m23 * z + m33;
+        matrix[12] = m00 * x + m10 * y + m30;
+        matrix[13] = m01 * x + m11 * y + m31;
     }
-
-    private scale (scaleX: number, scaleY: number)
-    {
-        const matrix = this.matrix;
-
-        matrix[0] *= scaleX;
-        matrix[1] *= scaleX;
-        matrix[2] *= scaleX;
-        matrix[3] *= scaleX;
-    
-        matrix[4] *= scaleY;
-        matrix[5] *= scaleY;
-        matrix[6] *= scaleY;
-        matrix[7] *= scaleY;
-    }
-
-    private rotate (angle: number)
-    {
-        const s: number = Math.sin(angle);
-        const c: number = Math.cos(angle);
-
-        const matrix = this.matrix;
-    
-        const m00 = matrix[0];
-        const m01 = matrix[1];
-        const m02 = matrix[2];
-        const m03 = matrix[3];
-        const m10 = matrix[4];
-        const m11 = matrix[5];
-        const m12 = matrix[6];
-        const m13 = matrix[7];
-
-        matrix[0] = m00 * c + m10 * s;
-        matrix[1] = m01 * c + m11 * s;
-        matrix[2] = m02 * c + m12 * s;
-        matrix[3] = m03 * c + m13 * s;
-        matrix[4] = m10 * c - m00 * s;
-        matrix[5] = m11 * c - m01 * s;
-        matrix[6] = m12 * c - m02 * s;
-        matrix[7] = m13 * c - m03 * s;
-    }
-
-    set x (value: number)
-    {
-        this._x = value;
-
-        this.translate(value, this._y);
-    }
-
-    get x (): number
-    {
-        return this._x;
-    }
-
-    set y (value: number)
-    {
-        this._y = value;
-
-        this.translate(this._x, value);
-    }
-
-    get y (): number
-    {
-        return this._y;
-    }
-
-    set rotation (value: number)
-    {
-        this._rotation = value;
-
-        this.rotate(value);
-    }
-
-    get rotation (): number
-    {
-        return this._rotation;
-    }
-
-    set scaleX (value: number)
-    {
-        this._scaleX = value;
-
-        this.scale(value, this._scaleY);
-    }
-
-    get scaleX (): number
-    {
-        return this._scaleX;
-    }
-
-    set scaleY (value: number)
-    {
-        this._scaleY = value;
-
-        this.scale(this._scaleX, value);
-    }
-
-    get scaleY (): number
-    {
-        return this._scaleY;
-    }
+    */
 }
